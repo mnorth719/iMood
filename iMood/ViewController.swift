cc045fb0b946893e4087e790c9f88b6c9c309d68
Matt North <matt.north93@gmail.com>
2017-08-17 16:24:45 -0700

    Committed WIP research code

--- iMood/ViewController.swift
+++ iMood/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
-
+    @IBOutlet fileprivate weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func selectImagePushed(_ sender: Any) {
+        if let hero = imageView.image?.cgImage {
+            let requestHandler = VNImageRequestHandler(cgImage: hero)
+            
+            
+            let visionRequest = VNDetectFaceLandmarksRequest() { (request, error) in
+                print(request.results)
+            }
+            
+            VNDet
+        }
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //todo: utilize vision api to find features
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = pickedImage
+        }
+    }
 }
 
